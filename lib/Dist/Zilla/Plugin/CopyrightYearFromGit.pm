4f617f65fd7c3f67f7e00f1f91a6eaa051987515
perlancar (on pc-office) <perlancar@gmail.com>
2019-01-30 14:02:06 +0700

    Add configuration: min_year

--- lib/Dist/Zilla/Plugin/CopyrightYearFromGit.pm
+++ lib/Dist/Zilla/Plugin/CopyrightYearFromGit.pm
@@ -12,6 +12,8 @@ with (
     'Dist::Zilla::Role::BeforeBuild',
 );
 
+has min_year => (is => 'rw');
+
 sub before_build {
     require Release::Util::Git;
     my $self = shift;
@@ -20,10 +22,15 @@ sub before_build {
     $self->log_fatal(["%s - %s"], $res->[0], $res->[1]) unless $res->[0] == 200;
 
     my $cur_year = (localtime)[5]+1900;
+
+    my $min_year = $self->min_year;
+    $min_year = $cur_year if defined $min_year && $min_year > $cur_year;
+
     my @years = @{ $res->[2] };
     if (!@years || $years[0] < $cur_year) {
         unshift @years, $cur_year;
     }
+    @years = grep { !defined($min_year) || $_ >= $min_year } @years;
     my $year = join(", ", sort {$b <=> $a} @years);
     $self->log(["Setting copyright_year to %s", $year]);
 
@@ -52,11 +59,19 @@ This plugin will set copyright year to something like:
  2017, 2015, 2014, 2013
 
 where the years will be retrieved from 1) the date of git tags that resemble
-version string (qr/^(version|ver|v)?\d/); 2) current date; and will be listed in descending
-order in a comma-separated list. This format is commonly used in books, where
-the year of each revision/edition is mentioned, e.g.:
+version string (qr/^(version|ver|v)?\d/); 2) current date; and will be listed in
+descending order in a comma-separated list. This format is commonly used in
+books, where the year of each revision/edition is mentioned, e.g.:
 
  Copyright (c) 2013, 2010, 2008, 2006 by Pearson Education, Inc.
 
+
+=head1 CONFIGURATION
+
+=head2 min_year
+
+Instruct the plugin to not include years below this year. Note that the current
+year will always be used, even though C<min_year> is (incorrectly) set to a
+value larger than the current year.
 
 =head1 SEE ALSO
